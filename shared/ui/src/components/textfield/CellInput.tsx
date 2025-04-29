c3f62cdc2942c86a92fe9eb0687a206a773d8c01
jisu-newid <125334728+jisu-newid@users.noreply.github.com>
2025-04-29 15:36:05 +0900

    👔 CellInput 컴포넌트에 reset 기능 추가 (#28)

--- shared/ui/src/components/textfield/CellInput.tsx
+++ shared/ui/src/components/textfield/CellInput.tsx
@@ -1,8 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, forwardRef, useImperativeHandle } from 'react';
 import { BoxComponentStyles, Size } from '@/types/common.ts';
 import InputBox from './InputBox.tsx';
 import IconReset from '@/assets/icReset.svg?react';
 import useCellInput from '@/components/textfield/cellInput.hooks.ts';
+
+export interface CellInputRef {
+    reset: () => void;
+}
 
 export interface CellInputProps {
     size?: Size;
@@ -17,58 +21,73 @@ export interface CellInputProps {
     className?: string;
 }
 
-const CellInput = ({
-    size = 'medium',
-    width = 0,
-    placeholder = 'Cell Field',
-    value: initialValue = '',
-    readOnly = false,
-    onChange = () => {},
-    onDone = () => {},
-    onEnter = () => {},
-    id = '',
-    className = '',
-}: CellInputProps) => {
-    const { inputRef, isFocused, value, isModified, handleChange, handleReset } = useCellInput({
-        initialValue,
-        onChange,
-        onDone,
-        onEnter,
-    });
+const CellInput = forwardRef<CellInputRef, CellInputProps>(
+    (
+        {
+            size = 'medium',
+            width = 0,
+            placeholder = 'Cell Field',
+            value: initialValue = '',
+            readOnly = false,
+            onChange = () => {},
+            onDone = () => {},
+            onEnter = () => {},
+            id = '',
+            className = '',
+        },
+        ref,
+    ) => {
+        const { inputRef, isFocused, value, isModified, handleChange, handleReset } = useCellInput({
+            initialValue,
+            onChange,
+            onDone,
+            onEnter,
+        });
 
-    const { height, textSizeClass } = BoxComponentStyles[size];
-    let stateClass = isFocused ? 'border-blue-500 bg-white' : 'border-transparent hover:border-grey-40';
-    if (readOnly) {
-        stateClass = 'border-transparent';
-    }
+        useImperativeHandle(
+            ref,
+            () => ({
+                reset: () => handleReset(),
+            }),
+            [handleReset],
+        );
 
-    const handleClearMouseDown = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-    }, []);
+        const { height, textSizeClass } = BoxComponentStyles[size];
+        let stateClass = isFocused ? 'border-blue-500 bg-white' : 'border-transparent hover:border-grey-40';
+        if (readOnly) {
+            stateClass = 'border-transparent';
+        }
 
-    return (
-        <InputBox.Root
-            className={`items-center justify-start gap-[8px] px-[11px] ${stateClass} ${className}`}
-            width={width}
-            height={height}
-        >
-            <InputBox.Field
-                ref={inputRef}
-                id={id}
-                className={`text-grey-90 ${textSizeClass}`}
-                placeholder={placeholder}
-                value={value}
-                readOnly={readOnly}
-                onChange={handleChange}
-            />
-            {isFocused && isModified && (
-                <InputBox.Suffix className={`size-[20px]`}>
-                    <button onMouseDown={handleClearMouseDown} onClick={handleReset}>
-                        <IconReset />
-                    </button>
-                </InputBox.Suffix>
-            )}
-        </InputBox.Root>
-    );
-};
+        const handleClearMouseDown = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+            e.preventDefault();
+        }, []);
+
+        return (
+            <InputBox.Root
+                className={`items-center justify-start gap-[8px] px-[11px] ${stateClass} ${className}`}
+                width={width}
+                height={height}
+            >
+                <InputBox.Field
+                    ref={inputRef}
+                    id={id}
+                    className={`text-grey-90 ${textSizeClass}`}
+                    placeholder={placeholder}
+                    value={value}
+                    readOnly={readOnly}
+                    onChange={handleChange}
+                />
+                {isFocused && isModified && (
+                    <InputBox.Suffix className={`size-[20px]`}>
+                        <button onMouseDown={handleClearMouseDown} onClick={handleReset}>
+                            <IconReset />
+                        </button>
+                    </InputBox.Suffix>
+                )}
+            </InputBox.Root>
+        );
+    },
+);
+
 export { CellInput };
+CellInput.displayName = 'CellInput';
