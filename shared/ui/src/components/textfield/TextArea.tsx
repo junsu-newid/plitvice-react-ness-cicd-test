b55d93bf9a930194852cdbce2b18c904184257af
hyungoo <hyungoo@its-newid.com>
2025-05-15 13:58:33 +0900

    :lipstick: TextArea 라벨 추가

--- shared/ui/src/components/textfield/TextArea.tsx
+++ shared/ui/src/components/textfield/TextArea.tsx
@@ -1,9 +1,15 @@
 import { useInput } from '@/hooks/useInput.ts';
 import React from 'react';
+import { BoxComponentStyles, Size } from '@/types/common.ts';
+
+type TextAreaSize = Extract<Size, 'medium' | 'large'>;
 
 interface TextAreaProps {
+    size?: TextAreaSize;
     width?: number;
     height?: number;
+    label?: string;
+    labelColor?: string;
     value?: string;
     readOnly?: boolean;
     disabled?: boolean;
@@ -14,8 +20,11 @@ interface TextAreaProps {
 }
 
 const TextArea = ({
+    size = 'medium',
     width = undefined,
     height = undefined,
+    label = '',
+    labelColor = '',
     value: initialValue,
     readOnly = false,
     disabled = false,
@@ -30,9 +39,10 @@ const TextArea = ({
     });
 
     const isInactive = readOnly || disabled;
-
     const containerClass = `py-[15px] pl-[15px] pr-[4px] border border-grey-40 rounded-[4px] ${isInactive ? 'bg-grey-20' : ' bg-white focus-within:border-blue-600'} ${className}`;
-    const textareaClass = `bg-inherit w-full h-full pr-[8px] overflow-auto scrollbar text-r16 text-grey-90 placeholder-grey-40 whitespace-pre-wrap break-words border-none resize-none outline-none`;
+    const textareaClass = `bg-inherit w-full h-full pr-[8px] overflow-auto scrollbar text-grey-90 placeholder-grey-40 whitespace-pre-wrap break-words border-none resize-none outline-none`;
+    const labelTextStyle = { color: labelColor || 'var(--color-grey-50)' };
+    const { labelSizeClass, textSizeClass } = BoxComponentStyles[size];
 
     return (
         <div
@@ -42,8 +52,13 @@ const TextArea = ({
             }}
             className={containerClass}
         >
+            {label !== '' ? (
+                <label className={`pl-[4px] ${labelSizeClass} non-draggable`} style={labelTextStyle}>
+                    {label}
+                </label>
+            ) : null}
             <textarea
-                className={textareaClass}
+                className={`${textareaClass} ${textSizeClass}`}
                 value={value}
                 readOnly={readOnly}
                 disabled={disabled}
