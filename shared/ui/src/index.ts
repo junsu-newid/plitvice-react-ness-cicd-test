2d6584cbd9d53ff5b39a120107b27c0adf314aa7
hyungoo <hyungoo@its-newid.com>
2025-07-07 10:36:31 +0900

    :lipstick: 정렬 아이콘 추가

--- shared/ui/src/index.ts
+++ shared/ui/src/index.ts
@@ -9,6 +9,7 @@ export { default as DefaultChipClearIcon } from './assets/icDefaultChipClear.svg
 export { default as DefaultChipCloseIcon } from './assets/icDefaultChipClose.svg?react';
 export { default as DragHandleIcon } from './assets/icDragHandle.svg?react';
 export { default as CalenderIcon } from './assets/icCalender.svg?react';
+export { default as SortingIcon } from './assets/icSorting.svg?react';
 
 export { SideNavBar } from '@/components/navigation/SideNavBar.tsx';
 export { TabMenu } from '@/components/navigation/TabMenu.tsx';
